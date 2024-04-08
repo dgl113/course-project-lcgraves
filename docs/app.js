1be3e54d381cc477f20b291af288f4cc9d891f4d
lcgraves <lgraves@northislandcollege.ca>
2024-04-08 16:13:48 -0700

    Add functionality to accordion

--- docs/app.js
+++ docs/app.js
@@ -17,3 +17,43 @@ function showPlants() {
 document.getElementById('scrollingText').addEventListener('click', function() {
     this.classList.toggle('paused');
   });
+
+
+// Add functionality to accordion
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Select all accordion buttons
+    var accButtons = document.querySelectorAll('.c-accordion__button');
+
+    // Iterate through each button
+    accButtons.forEach(function(btn) {
+      btn.addEventListener('click', function() {
+          // Check if this panel is already open
+          var isOpen = this.classList.contains('accordion-active');
+
+          // Use data-target attribute to find the panel
+          var panelId = this.getAttribute('data-target');
+          var panel = document.getElementById(panelId);
+
+          // First, remove 'active' class from all buttons and close all panels
+          accButtons.forEach(function(otherBtn) {
+              otherBtn.classList.remove('accordion-active');
+          });
+          document.querySelectorAll('.c-accordion__panel').forEach(function(otherPanel) {
+              otherPanel.style.maxHeight = null;
+          });
+
+          // Then, if the panel was not already open, open it and add 'active' class
+          if (!isOpen) {
+              this.classList.add('accordion-active'); // Add 'active' class to the clicked button
+              panel.style.maxHeight = panel.scrollHeight + "px"; // Open the panel
+          }
+      });
+  });
+
+   // Automatically open the first panel
+   if(accButtons.length > 0) {
+    // Force click the first button to open the first panel
+    accButtons[0].click();
+   }
+});
