37e8d32ff90971e3868afaf3c897b772a335540d
lcgraves <lgraves@northislandcollege.ca>
2024-04-08 15:53:19 -0700

    Rename javascript file

--- docs/app.js
+++ docs/app.js
@@ -1,1 +1,14 @@
+// Navigation
 
+document.getElementById("plants").addEventListener("click", showPlants);
+document.getElementById("hamburger").addEventListener("click", showNav);
+
+function showNav() {
+    var element = document.getElementById("nav-items");
+    element.classList.toggle("show-items");
+}
+
+function showPlants() {
+    var plantstuff = document.getElementById("plant-items");
+    plantstuff.classList.toggle("show-plants");
+}
